896b6e16d035180d0071cef70b590689ced10462
kevin <kevinteng2000@gmail.com>
2021-09-14 23:50:24 +0800

    'update'

--- Basic/data range.cpp
+++ Basic/data range.cpp
@@ -1,9 +1,6 @@
-int → (-2,147,483,648 to 2,147,483,647)
-unsigned int → (0 to 4,294,967,295)
-long → (-2,147,483,648 to 2,147,483,647)
-unsigned long → (0 to 4,294,967,295)
-long long → (-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
-unsigned long long → (0 to 18,446,744,073,709,551,615)
-
-ios_base::sync_with_stdio(0); 
-cin.tie(0);
+int (-2147483648 to 2147483647)
+unsigned int(0 to 4294967295)
+long(-2147483648 to 2147483647)
+unsigned long(0 to 4294967295)
+long long(-9223372036854775808 to 9223372036854775807)
+unsigned long long (0 to 18446744073709551615)
