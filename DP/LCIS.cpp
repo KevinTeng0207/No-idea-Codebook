cd3c2a2dad242ddbec5161f01cf7f55c72b75214
kevin <kevinteng2000@gmail.com>
2021-09-24 03:11:53 +0800

    update byu kevin

---
+++ DP/LCIS.cpp
@@ -0,0 +1,30 @@
+int LCIS_len(vector<int> arr1, vetor<int> arr2)
+{
+    int n = arr1.size(), m = arr2.size();
+    vector<int> table(m, 0);
+    for (int j = 0; j < m; j++)
+        table[j] = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int current = 0;
+
+        for (int j = 0; j < m; j++)
+        {
+
+            if (arr1[i] == arr2[j])
+                if (current + 1 > table[j])
+                    table[j] = current + 1;
+
+            if (arr1[i] > arr2[j])
+                if (table[j] > current)
+                    current = table[j];
+        }
+    }
+    int result = 0;
+    for (int i = 0; i < m; i++)
+        if (table[i] > result)
+            result = table[i];
+
+    return result;
+}
