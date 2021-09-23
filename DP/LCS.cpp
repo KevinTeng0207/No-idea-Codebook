0e595158ef9e6c82e8f337c26d95be25cb031659
kevin <kevinteng2000@gmail.com>
2021-09-24 03:28:08 +0800

    LCS print

--- DP/LCS.cpp
+++ DP/LCS.cpp
@@ -1,25 +1,26 @@
-int LCS(vector<int> Ans, vector<int> num) // Ans 跟 num 都要 index 從1開始放
+int LCS(vector<string> Ans, vector<string> num)
 {
-    vector<vector<int>> LCS(N + 1, vector<int>(N + 1, 0));
+    int N = Ans.size(), M = num.size();
+    vector<vector<int>> LCS(N + 1, vector<int>(M + 1, 0));
     for (int i = 1; i <= N; ++i)
     {
-        for (int j = 1; j <= N; ++j)
+        for (int j = 1; j <= M; ++j)
         {
-            if (Ans[i] == num[j])
+            if (Ans[i - 1] == num[j - 1])
                 LCS[i][j] = LCS[i - 1][j - 1] + 1;
             else
                 LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1]);
         }
     }
-    // printf("%d\n", LCS[N][N]);
-    return LCS[N][N];
-    //列印 LCS
-    vector<int> k;
+    cout << LCS[N][M] << '\n';
+    //列印 Last appear LCS
+    int n = N, m = M;
+    vector<string> k;
     while (n && m)
     {
         if (LCS[n][m] != max(LCS[n - 1][m], LCS[n][m - 1]))
         {
-            k.push_back(arr1[n]);
+            k.push_back(Ans[n - 1]);
             n--;
             m--;
         }
@@ -28,6 +29,9 @@ int LCS(vector<int> Ans, vector<int> num) // Ans 跟 num 都要 index 從1開始
         else if (LCS[n][m] == LCS[n][m - 1])
             m--;
     }
-
     reverse(k.begin(), k.end());
+    for (auto i : k)
+        cout << i << " ";
+    cout << endl;
+    return LCS[N][M];
 }
