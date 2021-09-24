3795ff11344e65b56c3ff01a9b16114ec5b51245
kevin <kevinteng2000@gmail.com>
2021-09-24 21:56:51 +0800

    || -> |

--- DP/Money problem.cpp
+++ DP/Money problem.cpp
@@ -5,7 +5,7 @@ void change(vector<int> price, int limit)
     c[0] = true;
     for (int i = 0; i < price.size(); ++i)      // 依序加入各種面額
         for (int j = price[i]; j <= limit; ++j) // 由低價位逐步到高價位
-            c[j] = c[j] || c[j - price[i]];     // 湊、湊、湊
+            c[j] = c[j] | c[j - price[i]];     // 湊、湊、湊
     if (c[limit]) cout << "YES\n";
     else cout << "NO\n";
 }
