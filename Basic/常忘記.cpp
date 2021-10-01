20c296351f1d19f4372a616fe25aa4ebc0977c5d
hsuanchia <wxes95301@gmail.com>
2021-10-01 22:15:37 +0800

    add binary search and unordered_set in 常忘記

--- Basic/常忘記.cpp
+++ Basic/常忘記.cpp
@@ -1,6 +1,8 @@
 round(double f); // 四捨五入
 ceil(double f);  // 無條件捨去
 floor(double f); //無條件進入
+vector<int>::iterator it = lower_bound(v.begin(), v.end(), val); //用binary search找大於或等於val的最小值的位置
+vector<int>::iterator it = upper_bound(v.begin(), v.end(), val); //用binary search找大於val的最小值的位置
 
 /*queue*/
 queue<datatype> q;
@@ -18,3 +20,11 @@ pop();   /*移掉最上面的值*/
 push();  /*新增值到最上面*/
 empty(); /*回傳bool,檢查是不是空的stack*/
 size();  /*stack 的大小*/
+
+/*unordered_set*/
+unordered_set<datatype> s; 
+unordered_set<datatype> s(arr,arr+n); /*initial with array*/
+insert(); /*插入值*/
+erase(); /*刪除值*/
+empty(); /*回傳bool,檢查是不是空的unordered_set*/
+count(); /*判斷元素是否存在 存在回傳1 反之則回傳0*/
