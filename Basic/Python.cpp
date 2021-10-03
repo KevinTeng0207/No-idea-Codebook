b54d469274ffdfc1fcfc5369d8529412b98f5b22
kevin <kevinteng2000@gmail.com>
2021-10-03 20:00:50 +0800

    update

--- Basic/Python.cpp
+++ Basic/Python.cpp
@@ -4,7 +4,7 @@ line = sys.stdin.readline()
 /*輸入2*/
 line = input().strip()
 D, R, N = map(int, line[:-1].split()) // 分三個 int 變數
-
+pow(a, b, c) // a ^ b % c
 print(*objects, sep = ' ', end = '\n')
 objects -- /*可以一次輸出多個對象*/
 sep -- /*分開多個objects*/
--- Mathematics/Hex to Dec.cpp
+++ Mathematics/Hex to Dec.cpp
@@ -2,26 +2,26 @@ int HextoDec(string num) //16 to 10
 {
     int base = 1;
     int temp = 0;
-    for (int i = num.length() - 1; i = 0; i--)
+    for (int i = num.length() - 1; i >= 0; i--)
     {
-        if (num[i] = '0' && num[i] = '9')
+        if (num[i] >= '0' && num[i] <= '9')
         {
-            temp += (num[i] - 48) base;
-            base = base 16;
+            temp += (num[i] - 48) * base;
+            base = base * 16;
         }
-        else if (num[i] = 'A' && num[i] = 'F')
+        else if (num[i] >= 'A' && num[i] <= 'F')
         {
-            temp += (num[i] - 55) base;
-            base = base 16;
+            temp += (num[i] - 55) * base;
+            base = base * 16;
         }
     }
     return temp;
 }
-void DecToHex(int p_intValue) //10 to 16
+void DecToHex(int p) //10 to 16
 {
-    char l_pCharRes = new (char);
-    sprintf(l_pCharRes, % X, p_intValue);
-    int l_intResult = stoi(l_pCharRes);
-    cout l_pCharRes n;
-    return l_intResult;
+    char *l = new (char);
+    sprintf(l, "%X", p);
+    //int l_intResult = stoi(l);
+    cout << l << "\n";
+    //return l_intResult;
 }
