79c4def50d65efd07be492eb6a76fe889937b666
kevin <kevinteng2000@gmail.com>
2021-10-23 18:13:45 +0800

    update

--- Other/Merge sort.cpp
+++ Other/Merge sort.cpp
@@ -1,48 +1,37 @@
-long long merge(vector<int> &P, int left, int mid, int right)
+long long merge(vector<int> &arr, int left, int mid, int right)
 {
-	int *tmp = new int[right - left + 1];
-	long long sum = 0;
-
-	int iLeft = left;
-	int iRight = mid + 1;
-	int iMerge = 0;
-
-	// the middle reverse numbers
-	while (iLeft <= mid && iRight <= right)
-	{
-		if (P[iLeft] <= P[iRight])
-		{
-			tmp[iMerge++] = P[iLeft++];
-		}
-		else
-		{
-			tmp[iMerge++] = P[iRight++];
-			sum += mid - iLeft + 1; // key point
-		}
-	}
-	while (iLeft <= mid)
-		tmp[iMerge++] = P[iLeft++];
-
-	while (iRight <= right)
-		tmp[iMerge++] = P[iRight++];
-
-	for (int i = left; i <= right; ++i)
-		P[i] = tmp[i - left];
-
-	delete[] tmp;
-	return sum;
+    int *tmp = new int[right - left + 1];
+    long long sum = 0;
+    int l = left, r = mid + 1, m = 0;
+    while (l <= mid && r <= right)
+    {
+        if (arr[l] <= arr[r])
+            tmp[m++] = arr[l++];
+        else
+        {
+            tmp[m++] = arr[r++];
+            sum += mid - l + 1;
+        }
+    }
+    while (l <= mid)
+        tmp[m++] = arr[l++];
+    while (r <= right)
+        tmp[m++] = arr[r++];
+    for (int i = left; i <= right; ++i)
+        arr[i] = tmp[i - left];
+    delete[] tmp;
+    return sum;
 }
-
-long long mergesort(vector<int> &P, int left, int right)
+long long mergesort(vector<int> &arr, int left, int right)
 {
-	long long sum = 0;
-	// left = 0, right = P.size() - 1
-	if (left < right)
-	{
-		int mid = (left + right) / 2;
-		sum = mergesort(P, left, mid);
-		sum += mergesort(P, mid + 1, right);
-		sum += merge(P, left, mid, right);
-	}
-	return sum; // 回傳為 swap 次數
+    long long sum = 0;
+    // left = 0, right = P.size() - 1
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+        sum = mergesort(arr, left, mid);
+        sum += mergesort(arr, mid + 1, right);
+        sum += merge(arr, left, mid, right);
+    }
+    return sum; // 回傳為 swap 次數
 }
