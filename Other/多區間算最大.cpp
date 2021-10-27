2203ad61b3dbfd13af942f48305b1381f0904a63
kevin <kevinteng2000@gmail.com>
2021-10-28 00:52:26 +0800

    update

---
+++ Other/多區間算最大.cpp
@@ -0,0 +1,21 @@
+bool name(pii a, pii b)
+{ return b.first > a.first;}
+vector<pii> data;
+data.pb(pii(a, c)); // 區間 a 到 c
+sort(data.begin(), data.end(), name); // pair first 從 小 到 大
+int l = data[0].x, r = data[0].y, res = 0;
+for (int i = 1; i < data.size(); i++)
+{
+    if (data[i].x <= r)
+    {
+        if (r < data[i].y)
+            r = data[i].y;
+    }
+    else
+    {
+        res += r - l;
+        l = data[i].x;
+        r = data[i].y;
+    }
+}
+res += r - l; // 最大段落不重疊
