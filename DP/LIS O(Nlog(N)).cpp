498d9705e88f7097ea0a90744c71b861d79f8334
kevin <kevinteng2000@gmail.com>
2021-11-01 21:17:09 +0800

    update

---
+++ DP/LIS O(Nlog(N)).cpp
@@ -0,0 +1,19 @@
+int LIS(vector<int> &v) // O(n*log(n))
+{
+    if (v.size() == 0)
+        return 0;
+    vector<int> tail(v.size(), 0);
+    int length = 1;
+    tail[0] = v[0];
+    for (int i = 1; i < v.size(); i++)
+    {
+        auto b = tail.begin(), e = tail.begin() + length;
+        auto it = lower_bound(b, e, v[i]);
+        if (it == tail.begin() + length)
+            tail[length++] = v[i];
+        else
+            *it = v[i];
+    }
+
+    return length;
+}
