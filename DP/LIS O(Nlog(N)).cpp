b99db87186ba59c2bea9a32ef417939c21e710f3
kevin <kevinteng2000@gmail.com>
2021-11-01 21:24:44 +0800

    update

--- DP/LIS O(Nlog(N)).cpp
+++ DP/LIS O(Nlog(N)).cpp
@@ -3,16 +3,16 @@ int LIS(vector<int> &v) // O(n*log(n))
   // 但必須注意 lower_bound or upper_bound
     if (v.size() == 0)
         return 0;
-    vector<int> tail(v.size(), 0);
+    vector<int> dp(v.size(), 0);
     int length = 1;
-    tail[0] = v[0];
+    dp[0] = v[0];
     for (int i = 1; i < v.size(); i++)
     {
-        auto b = tail.begin(), e = tail.begin() + length;
+        auto b = dp.begin(), e = dp.begin() + length;
         // auto it = lower_bound(b, e, v[i]); // 後面 >= 前面
         auto it = upper_bound(b, e, v[i]); // 後面 > 前面
-        if (it == tail.begin() + length)
-            tail[length++] = v[i];
+        if (it == dp.begin() + length)
+            dp[length++] = v[i];
         else
             *it = v[i];
     }
